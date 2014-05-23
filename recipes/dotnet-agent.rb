f64762aa789b25def0144a2bb69c5e6ca3aa2650
Joshua Benjamin <jbenjamin@precisiondev.net>
2014-05-22 17:15:36 -0700

    Added a template to configure the .net agent

--- recipes/dotnet-agent.rb
+++ recipes/dotnet-agent.rb
@@ -17,3 +17,27 @@ windows_package 'Install New Relic .NET Agent' do
   action :install
   not_if { File.exist?('C:\\Program Files\\New Relic\\.NET Agent') }
 end
+
+template node['newrelic']['dotnet-agent']['config_file'] do
+  source 'newrelic.config.erb'
+  variables(
+    :license => license,
+    :appname => node['newrelic']['application_monitoring']['appname'],
+    :enabled => node['newrelic']['application_monitoring']['enabled'],
+    :logfile => node['newrelic']['application_monitoring']['logfile'],
+    :loglevel => node['newrelic']['application_monitoring']['loglevel'],
+    :daemon_ssl => node['newrelic']['application_monitoring']['daemon']['ssl'],
+    :capture_params => node['newrelic']['application_monitoring']['capture_params'],
+    :ignored_params => node['newrelic']['application_monitoring']['ignored_params'],
+    :transaction_tracer_enable => node['newrelic']['application_monitoring']['transaction_tracer']['enable'],
+    :transaction_tracer_threshold => node['newrelic']['application_monitoring']['transaction_tracer']['threshold'],
+    :transaction_tracer_record_sql => node['newrelic']['application_monitoring']['transaction_tracer']['record_sql'],
+    :transaction_tracer_stack_trace_threshold => node['newrelic']['application_monitoring']['transaction_tracer']['stack_trace_threshold'],
+    :transaction_tracer_slow_sql => node['newrelic']['application_monitoring']['transaction_tracer']['slow_sql'],
+    :transaction_tracer_explain_threshold => node['newrelic']['application_monitoring']['transaction_tracer']['explain_threshold'],
+    :error_collector_enable => node['newrelic']['application_monitoring']['error_collector']['enable'],
+    :browser_monitoring_auto_instrument => node['newrelic']['application_monitoring']['browser_monitoring']['auto_instrument'],
+    :cross_application_tracer_enable => node['newrelic']['application_monitoring']['cross_application_tracer']['enable']
+  )
+  action :create
+end
